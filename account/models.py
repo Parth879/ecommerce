62d223ed3dbf818fe27aa18663e900afbc75588f
Parth879 <parthnashit682@gmail.com>
2024-12-19 16:00:25 +0530

    'news'

---
+++ account/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+# Create your models here.
+
+class AccountManager(BaseUserManager):
+    def create_user(self,first_name, last_name, username, email, password=None):
+
+        if not email:
+            raise ValueError("Email is Must For Registration.")
+
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name
+        )
+        user.set_password = password
+        user.save()
+
+    def create_superuser(self,first_name, last_name, username, email, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+            password = password
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_active = True
+        user.is_superadmin = True
+        user.save()
+
+
+class Account(AbstractBaseUser):
+    email           = models.EmailField(max_length=100, unique=True)
+    username        = models.CharField(max_length=50)
+    first_name      = models.CharField(max_length=50)
+    last_name       = models.CharField(max_length=50)
+    phone_number    = models.CharField(max_length=50)
+
+    created_at      = models.DateTimeField(auto_now_add=True)
+    is_admin        = models.BooleanField(default=False)
+    is_staff        = models.BooleanField(default=False)
+    is_active       = models.BooleanField(default=False)
+    is_superadmin   = models.BooleanField(default=False)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username','first_name','last_name']
+
+    def has_module_perm(self,admin):
+        self.is_admin = True
