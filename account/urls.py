5344510e03a0c02bf9adfdeed290b745c82a27c0
Parth879 <parthnashit682@gmail.com>
2024-12-24 15:38:16 +0530

    chnages

--- account/urls.py
+++ account/urls.py
@@ -3,4 +3,5 @@ from account.views import *
 
 urlpatterns = [
     path('register',register,name='register'),
+    path('login',login,name='login'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from account.forms import RegistrationForm
+from account.models import Account
+from django.contrib import auth
 # Create your views here.
 
 def register(request):
-    form = RegistrationForm()
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data['password']
+            confirm_password = form.cleaned_data['confirm_password']
+
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,password=password,username=username)
+            user.phone_number = phone_number
+            user.save()
+            return redirect('index')
+        
+    else:
+        form = RegistrationForm()
 
     context = {
         'form':form,
     }
     return render(request,'accounts/register.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['Username']
+        password = request.POST['Password']
+
+        user = auth.authenticate(request,username=username,password=password)
+        
+        if user is not None:
+            auth.login(request,user)
+            return redirect('index')
+        
+    return render(request,'accounts/login.html')
