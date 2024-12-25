5f245f668c936e9ab7a6af439422ddbf5d4a607b
Parth879 <parthnashit682@gmail.com>
2024-12-25 16:51:08 +0530

    registrations

--- account/urls.py
+++ account/urls.py
@@ -4,4 +4,6 @@ from account.views import *
 urlpatterns = [
     path('register',register,name='register'),
     path('login',login,name='login'),
+    path('activate/<uid64>/<token>',activate,name='activate'),
+  
 ]
--- account/views.py
+++ account/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render,redirect
 from account.forms import RegistrationForm
 from account.models import Account
 from django.contrib import auth
+
+# Email
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+
+from django.http import HttpResponse
+
 # Create your views here.
 
 def register(request):
@@ -14,11 +25,24 @@ def register(request):
             email = form.cleaned_data.get('email')
             password = form.cleaned_data['password']
             confirm_password = form.cleaned_data['confirm_password']
-
             username = email.split("@")[0]
             user = Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,password=password,username=username)
             user.phone_number = phone_number
             user.save()
+            
+            email_subject = "Please Activate Your Account."
+            current_side = get_current_site(request)
+
+            message = render_to_string('accounts/verfication_mail.html',{
+                'user':user,
+                'domain' : current_side,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : default_token_generator.make_token(user),
+            })
+
+            send_email = EmailMessage(email_subject,message,to=[email])
+            send_email.send()
+
             return redirect('index')
         
     else:
@@ -42,3 +66,19 @@ def login(request):
             return redirect('index')
         
     return render(request,'accounts/login.html')
+
+
+def activate(request,uid64,token):
+    try:
+        userid = urlsafe_base64_decode(uid64).decode()
+        user = Account._default_manager.get(id=userid)
+        tokens = default_token_generator.check_token(user,token)
+    except:
+        user = None
+    
+    if user is not None and tokens:
+        user.is_active = True
+        user.save()
+        return redirect('login')
+    else:
+        return redirect('register')
